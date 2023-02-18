be45f01d373440baabbfb6c2edd35d4c0aba059c
Gallzor <s1181531@student.windesheim.nl>
2023-02-18 19:05:59 +0100

    finished IfElseopdracht

--- Program.cs
+++ Program.cs
@@ -25,28 +25,39 @@
             Console.WriteLine(tekstbestand);
         }
 
+        //2e gedeelte opdracht: "Check of valide path word uitgevoerd" en
+        //"Check of de file bestaat en de juiste extensie heeft: txt"
         static void Opdracht2IfElse()
         {
 
             string path = "C:\\Users\\gally\\Documents\\SCHOOL\\CODING\\CSHARP\\ws1.txt";
 
-            if(string.IsNullOrWhiteSpace(path) || path.Length < 3)
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                Console.WriteLine("Seems like you filled something it. Let's check if it's correct, shall we?");
+                Console.ReadKey();
+            }   
+            if (path.Contains(".txt"))
             {
                 string tekstbestand = File.ReadAllText(path);
+                Console.WriteLine("The file is made out of .txt atleast.");
+                File.Exists(path);
+                Console.ReadKey();
+                Console.WriteLine("And the source file exists! Guess you want to read the file now, huh?");
+                Console.ReadKey();
                 Console.WriteLine(tekstbestand);
             }
 
-            else if(!File.Exists(path))
+            else if(File.Exists(path))
             {
                 string tekstbestand = File.ReadAllText(path);
-                Console.WriteLine(tekstbestand);
+               
             }
 
             else
             {
                 Console.WriteLine("Seems like the file or path has been taken to another castle..");
             }
-
         }
 
         // Opdracht 1 uit de Powerpoint (slide 14/28)
