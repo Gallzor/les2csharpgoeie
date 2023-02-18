0127e8b66f48039492dde960720f305d33dee734
Gallzor <s1181531@student.windesheim.nl>
2023-02-18 18:15:37 +0100

    finished opdracht1

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@
 
             Opdracht1();
         }
+
+       // Opdracht 1 uit de Powerpoint (slide 14/28)
         static void Opdracht1()
         {
             Console.WriteLine("Hello C#sharp lesson!");
@@ -20,12 +22,12 @@
             string name = Console.ReadLine();
             Console.WriteLine("And what is your age?");
             int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("You name is {0}, and your age is {1}!");
+            Console.WriteLine("Your name is {0} and your age is {1}", name, age);
             Console.ReadKey();
-            Console.WriteLine("Could you give me your lastname as well?");
+            Console.WriteLine("Could you give me your last name as well?");
             string lastname = Console.ReadLine();
             int years = 65 - age;
-            Console.WriteLine("Dear {2}, in {3} years, you will be 65+ years old!");
+            Console.WriteLine("Dear {0}, in {1} years, you will be 65+ years old!", lastname, years);
             Console.WriteLine("..And you will receive a 65+ pass. Congrats!");
         }
 
