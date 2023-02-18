61ce1b5b0e421f7733fe634a826c39f30d8c9168
Gallzor <s1181531@student.windesheim.nl>
2023-02-18 12:59:24 +0100

    Trying to fix Opdracht 1 errors

--- Program.cs
+++ Program.cs
@@ -11,19 +11,23 @@
             //hier maak je de = new var();
             //writeline ook hier, vergeet de dollarsign niet($"");
 
-            static void Opdracht1 ()
-            {
-                Console.WriteLine("Hello C#sharp lesson!");
-                Console.WriteLine("Can you give me your name?");
-                string name = Console.ReadLine();
-                Console.WriteLine("And what is your age?");
-                int age = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("You name is " + name + " and your age is " + age + "!");
-                Console.ReadKey(); Console.WriteLine("Could you give me your lastname as well?");
-                string lastname = Console.ReadLine();
-                int years = 65 - age; Console.WriteLine("Dear " + lastname + ", " + name + ", in " + years + " years, you will be 65+ years old");
-                Console.WriteLine("And you will receive a 65+ pass. Congrats!");
-            }
+            Opdracht1();
         }
+        static void Opdracht1()
+        {
+            Console.WriteLine("Hello C#sharp lesson!");
+            Console.WriteLine("Can you give me your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("And what is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You name is {0}, and your age is {1}!");
+            Console.ReadKey();
+            Console.WriteLine("Could you give me your lastname as well?");
+            string lastname = Console.ReadLine();
+            int years = 65 - age;
+            Console.WriteLine("Dear {2}, in {3} years, you will be 65+ years old!");
+            Console.WriteLine("..And you will receive a 65+ pass. Congrats!");
+        }
+
     }
 }
